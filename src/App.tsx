fbe759f1a6ae7e9f264b783e4ebf65d4a359d2d5
Axel Örn Sigurðsson <axel@absalon.is>
2020-05-16 19:01:53 +0000

    Add default example as initial state

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,34 @@ import './App.css';
 
 const THEME: string = 'github';
 
+const EXAMPLE: string = `{
+  "number_int": 123,
+  "number_float": 3.0,
+  "string": "string",
+  "list_single_type": ["a", "b", "c"],
+  "list_mixed_type": ["1", 2, 3.0],
+  "nested_dict": {
+    "number": 1,
+    "string": "value"
+  },
+  "same_nested_dict": {
+    "number": 2,
+    "string": "different value"
+  },
+  "multipe_levels": {
+    "level2": {
+      "level3": {
+        "number": 3,
+        "string": "more values"
+      }
+    }
+  },
+  "nested_invalid": { "numeric-id": 123, "from": "far away" },
+  "optional_items": [1, 2, "3", "4", null, 5, 6, null]
+}`;
+
 const App = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(EXAMPLE);
   const [output, setOutput] = useState('');
   const [validJson, setValidJson] = useState(true);
   const [annotations, setAnnotations] = useState([] as IAnnotation[]);
